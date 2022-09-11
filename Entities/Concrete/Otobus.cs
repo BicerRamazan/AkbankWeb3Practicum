829fea4214f6374c95af2488e8bc67faff0b35eb
Ramazan Biçer <77917717+BicerRamazan@users.noreply.github.com>
2022-09-11 17:37:36 +0300

    Add project files.

---
+++ Entities/Concrete/Otobus.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Otobus:IEntity
+    {
+        public int HgsNumarasi { get; set; }
+        public string AdSoyad { get; set; }
+        public int AracSinifi { get; set; }
+        public DateTime GecisTarihi { get; set; }
+        public decimal Bakiye { get; set; }
+    }
+}
